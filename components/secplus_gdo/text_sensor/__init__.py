867afaf3cf86acafb19188c4799938635c40f948
John <34163498+CircuitSetup@users.noreply.github.com>
2025-08-11 11:29:38 -0400

    Update __init__.py

--- components/secplus_gdo/text_sensor/__init__.py
+++ components/secplus_gdo/text_sensor/__init__.py
@@ -1,6 +1,6 @@
 """
 /*
- * Copyright (C) 2024  Konnected Inc.
+ * Copyright (C) 2025 CircuitSetup
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
